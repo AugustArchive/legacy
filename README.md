710fff068213f207d13760714e025a48b55be6bd
August (Chris) <augu@voided.pw>
2019-01-14 19:32:27 -0700

    Push legacy build

---
+++ README.md
@@ -0,0 +1,5 @@
+# Legacy Build
+:warning: **This is a legacy build, it requires RethinkDB to run!**
+
+## Why is this here?
+To basically reuse code lol
---
+++ src/deps/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    valid: require('./valid-url'),
+    entities: require('./entities'),
+    humanize: require('./humanize-duration/humanize'),
+    dateformat: require('./dateformat/dateformat'),
+    osu: require('./osu'),
+    kitsu: require('./kitsu'),
+    weeb: require('./weeb.sh')
+};
